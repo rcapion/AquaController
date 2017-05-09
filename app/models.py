7d001113bed0b40ab69e9b14ce3f1a8b5cdfa04a
Richard Capion <richard@capion.org>
2017-05-09 13:45:11 +0200

    Added model for RelayScenarios

--- app/models.py
+++ app/models.py
@@ -1,34 +1,39 @@
 from app import app, db
 from flask_login import UserMixin
 
-class RelayGroups(db.Model):
+class RelayGroup(db.Model):
     # Database model
-    __tablename__ = 'relaygroups'
+    __tablename__ = 'relaygroup'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     relays = db.relationship('Relay', backref='group', lazy='dynamic')
 
     def __repr__(self):
-        return '<Group {} - {}>'.format(self.id, self.name)
+        return '<RelayGroup {} - {}>'.format(self.id, self.name)
 
 
 class Relay(db.Model):
         # Database model
         __tablename__ = 'relay'
-        id         = db.Column(db.Integer, primary_key=True)
-        name       = db.Column(db.String(50), unique=True)
-        pin        = db.Column(db.Integer, nullable=True, unique=True)
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(50), unique=True)
+        pin = db.Column(db.Integer, nullable=True, unique=True)
         connection = db.Column(db.String(2))
-        state      = db.Column(db.Boolean, nullable=True)
+        state = db.Column(db.Boolean, nullable=True)
         group_id = db.Column(db.Integer, db.ForeignKey('relaygroups.id'))
 
         def __repr__(self):
             return '<Relay {} - {}>'.format(self.id, self.name)
 
-        # def __init__(self):
-        #     # Parse configfile
-        #     self.config = configparser.ConfigParser()
-        #     self.config.read('RelaySetup.ini')
+class RelayScenario(db.Model):
+    # Database model
+    __tablename__ = 'relayscenario'
+    id = db.Column(db.Integer)
+    relay_id = db.Column(db.Integer, db.ForeignKey('relay.id'))
+    state = db.Column(db.Boolean, nullable=True)
+
+    def __repr__(self):
+        return '<RelayScenario {} - {}, {}>'.format(self.id, self.relay_id, self.state)
 
 class User(UserMixin):
 
